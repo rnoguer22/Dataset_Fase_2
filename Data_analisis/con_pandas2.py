25e2d17f0183e265818867de69b4b86b2977828f
Pelayo <phuermij@myuax.com>
2022-04-16 17:31:42 +0200

    leer un archivo csv

--- Data_analisis/con_pandas2.py
+++ Data_analisis/con_pandas2.py
@@ -8,3 +8,9 @@ columnas = {"columna 1": ["datos de la columna"], "columna 2": ["datos de la col
 archivo = pd.DataFrame(data=columnas)
 #Esto nos devolverá una tabla con los datos proporcionados anteriormente
 print(archivo)
+
+#Para leer un archivo csv con pandas usamons el método pd.read_csv:
+wineqt = pd.read_csv("WineQT.csv") #Para leerlo desde un archivo
+wineqt_link = pd.read_csv(r"https://github.com/rnoguer22/Dataset_Fase_2/blob/main/WineQT.csv") #Para leerlo desde una URL
+
+
