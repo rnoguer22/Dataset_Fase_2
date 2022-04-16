f0f9e7f2a01c7b9c557b63ed1ac9773659590ec4
Pelayo <phuermij@myuax.com>
2022-04-16 17:06:46 +0200

    crear archivo csv

--- Data_analisis/con_pandas2.py
+++ Data_analisis/con_pandas2.py
@@ -1,2 +1,10 @@
 #Importamos pandas
 import pandas as pd
+
+#Crear, leer y escribir
+#Para crear un archivo csv con pandas usamos el método pd.DataFrame:
+
+columnas = {"columna 1": ["datos de la columna"], "columna 2": ["datos de la columna"]}
+archivo = pd.DataFrame(data=columnas)
+#Esto nos devolverá una tabla con los datos proporcionados anteriormente
+print(archivo)
